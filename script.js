44d7b7a31a35742761c54aa0f52b4124479a6458
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-03-18 12:51:49 -0300

    adicionando o arquivo js
    este arquivo vai se comunicar com a api do github

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const avatar = document.getElementById('avatar')
+    const perfilName = document.getElementById('perfil-name')
+    const perfilUserName = document.getElementById('perfil-username')
+    const repositórios = document.getElementById('Repositórios')
+    const seguidores = document.getElementById('seguidores')
+    const seguindo = document.getElementById('seguindo')
+    const link = document.getElementById('link')
+
+    fetch('https://api.github.com/users/JulioVieiraP')
+    .then(function (resposta) {
+        return resposta.json()
+    })
+    .then(function(json) {
+        avatar.src = json.avatar_url
+        perfilName.innerText = json.name
+        perfilUserName.innerText= json.login
+        repositórios.innerText  = json.public_repos
+        seguidores.innerText = json.following
+        seguindo.innerText = json.followers
+        link.href = json.html_url
+    })
+    
+    
+})
